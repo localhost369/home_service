75f2ba9ae9bab0d78cecdeb39d7c7fdf85023677
Rakesh Das <rakeshdas0804@gmail.com>
2023-02-19 15:57:48 +0530

    initialize

---
+++ lib/app/repositories/payment_repository.dart
@@ -0,0 +1,46 @@
+import 'package:get/get.dart';
+
+import '../models/booking_model.dart';
+import '../models/payment_method_model.dart';
+import '../models/payment_model.dart';
+import '../providers/laravel_provider.dart';
+
+class PaymentRepository {
+  LaravelApiClient _laravelApiClient;
+
+  PaymentRepository() {
+    _laravelApiClient = Get.find<LaravelApiClient>();
+  }
+
+  Future<List<PaymentMethod>> getMethods() {
+    return _laravelApiClient.getPaymentMethods();
+  }
+
+  Future<Payment> create(Booking booking) {
+    return _laravelApiClient.createPayment(booking);
+  }
+
+  String getPayPalUrl(Booking booking) {
+    return _laravelApiClient.getPayPalUrl(booking);
+  }
+
+  String getRazorPayUrl(Booking booking) {
+    return _laravelApiClient.getRazorPayUrl(booking);
+  }
+
+  String getStripeUrl(Booking booking) {
+    return _laravelApiClient.getStripeUrl(booking);
+  }
+
+  String getPayStackUrl(Booking booking) {
+    return _laravelApiClient.getPayStackUrl(booking);
+  }
+
+  String getFlutterWaveUrl(Booking booking) {
+    return _laravelApiClient.getFlutterWaveUrl(booking);
+  }
+
+  String getStripeFPXUrl(Booking booking) {
+    return _laravelApiClient.getStripeFPXUrl(booking);
+  }
+}
