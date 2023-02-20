903b7901153a3073e16992b9fc79186b77864a87
Rakesh Das <rakeshdas0804@gmail.com>
2023-02-20 13:12:27 +0530

    minor fix

--- lib/app/modules/category/widgets/services_list_item_widget.dart
+++ lib/app/modules/category/widgets/services_list_item_widget.dart
@@ -3,6 +3,7 @@
  */
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -10,7 +11,7 @@ import '../../../../common/ui.dart';
 import '../../../models/e_service_model.dart';
 import '../../../routes/app_routes.dart';
 
-class ServicesListItemWidget extends StatelessWidget {
+class ServicesListItemWidget extends StatefulWidget {
   const ServicesListItemWidget({
     Key key,
     @required EService service,
@@ -20,188 +21,262 @@ class ServicesListItemWidget extends StatelessWidget {
   final EService _service;
 
   @override
+  State<ServicesListItemWidget> createState() => _ServicesListItemWidgetState();
+}
+
+class _ServicesListItemWidgetState extends State<ServicesListItemWidget> {
+  int quantity = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Get.toNamed(Routes.E_SERVICE, arguments: {'eService': _service, 'heroTag': 'service_list_item'});
-      },
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        decoration: Ui.getBoxDecoration(),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      decoration: Ui.getBoxDecoration(),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GestureDetector(
+            onTap: () {
+              // Get.toNamed(Routes.E_SERVICE,
+              //     arguments: {'eService': _service, 'heroTag': 'service_list_item'});
+              Get.toNamed(Routes.BOOK_E_SERVICE,
+                  arguments: {'eService': widget._service, 'quantity': 1});
+            },
+            child: Column(
               children: [
                 Hero(
-                  tag: 'service_list_item' + _service.id,
+                  tag: 'service_list_item' + widget._service.id,
                   child: ClipRRect(
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10)),
                     child: CachedNetworkImage(
                       height: 80,
                       width: 80,
                       fit: BoxFit.cover,
-                      imageUrl: _service.firstImageUrl,
+                      imageUrl: widget._service.firstImageUrl,
                       placeholder: (context, url) => Image.asset(
                         'assets/img/loading.gif',
                         fit: BoxFit.cover,
                         width: double.infinity,
                         height: 80,
                       ),
-                      errorWidget: (context, url, error) => Icon(Icons.error_outline),
+                      errorWidget: (context, url, error) =>
+                          Icon(Icons.error_outline),
                     ),
                   ),
                 ),
-                if (_service.eProvider.available)
+                if (widget._service.eProvider.available)
                   Container(
                     width: 80,
                     child: Text("Available".tr,
                         maxLines: 1,
                         style: Get.textTheme.bodyText2.merge(
-                          TextStyle(color: Colors.green, height: 1.4, fontSize: 10),
+                          TextStyle(
+                              color: Colors.green, height: 1.4, fontSize: 10),
                         ),
                         softWrap: false,
                         textAlign: TextAlign.center,
                         overflow: TextOverflow.fade),
                     decoration: BoxDecoration(
                       color: Colors.green.withOpacity(0.2),
-                      borderRadius: BorderRadius.only(bottomRight: Radius.circular(10), bottomLeft: Radius.circular(10)),
+                      borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(10),
+                          bottomLeft: Radius.circular(10)),
                     ),
                     padding: EdgeInsets.symmetric(horizontal: 5, vertical: 6),
                   ),
-                if (!_service.eProvider.available)
+                if (!widget._service.eProvider.available)
                   Container(
                     width: 80,
                     child: Text("Offline".tr,
                         maxLines: 1,
                         style: Get.textTheme.bodyText2.merge(
-                          TextStyle(color: Colors.grey, height: 1.4, fontSize: 10),
+                          TextStyle(
+                              color: Colors.grey, height: 1.4, fontSize: 10),
                         ),
                         softWrap: false,
                         textAlign: TextAlign.center,
                         overflow: TextOverflow.fade),
                     decoration: BoxDecoration(
                       color: Colors.grey.withOpacity(0.2),
-                      borderRadius: BorderRadius.only(bottomRight: Radius.circular(10), bottomLeft: Radius.circular(10)),
+                      borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(10),
+                          bottomLeft: Radius.circular(10)),
                     ),
                     padding: EdgeInsets.symmetric(horizontal: 5, vertical: 6),
                   ),
               ],
             ),
-            SizedBox(width: 12),
-            Expanded(
-              child: Wrap(
-                runSpacing: 10,
-                alignment: WrapAlignment.start,
-                children: <Widget>[
-                  Row(
-                    children: [
-                      Text(
-                        _service.name ?? '',
-                        style: Get.textTheme.bodyText2,
-                        maxLines: 3,
-                        // textAlign: TextAlign.end,
-                      ),
-                    ],
-                  ),
-                  Divider(height: 8, thickness: 1),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Wrap(
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        spacing: 5,
-                        children: [
-                          // SizedBox(
-                          //   height: 32,
-                          //   child: Chip(
-                          //     padding: EdgeInsets.all(0),
-                          //     label: Row(
-                          //       mainAxisAlignment: MainAxisAlignment.center,
-                          //       children: <Widget>[
-                          //         Icon(
-                          //           Icons.star,
-                          //           color: Get.theme.accentColor,
-                          //           size: 18,
-                          //         ),
-                          //         Text(_service.rate.toString(), style: Get.textTheme.bodyText2.merge(TextStyle(color: Get.theme.accentColor, height: 1.4))),
-                          //       ],
-                          //     ),
-                          //     backgroundColor: Get.theme.accentColor.withOpacity(0.15),
-                          //     shape: StadiumBorder(),
-                          //   ),
-                          // ),
-                          // Text(
-                          //   "From (%s)".trArgs([_service.totalReviews.toString()]),
-                          //   style: Get.textTheme.bodyText1,
-                          // ),
-                        ],
-                      ),
-                      Ui.getPrice(_service.price, style: Get.textTheme.headline6),
-                    ],
-                  ),
-                  // Row(
-                  //   children: [
-                  //     Icon(
-                  //       Icons.build_circle_outlined,
-                  //       size: 18,
-                  //       color: Get.theme.focusColor,
-                  //     ),
-                  //     SizedBox(width: 5),
-                  //     Flexible(
-                  //       child: Text(
-                  //         _service.eProvider.name,
-                  //         maxLines: 1,
-                  //         overflow: TextOverflow.fade,
-                  //         softWrap: false,
-                  //         style: Get.textTheme.bodyText1,
-                  //       ),
-                  //     ),
-                  //   ],
-                  // ),
-                  // Row(
-                  //   children: [
-                  //     Icon(
-                  //       Icons.place_outlined,
-                  //       size: 18,
-                  //       color: Get.theme.focusColor,
-                  //     ),
-                  //     SizedBox(width: 5),
-                  //     Flexible(
-                  //       child: Text(
-                  //         // TODO eProvider address
-                  //         _service.eProvider.firstAddress,
-                  //         maxLines: 1,
-                  //         overflow: TextOverflow.fade,
-                  //         softWrap: false,
-                  //         style: Get.textTheme.bodyText1,
-                  //       ),
-                  //     ),
-                  //   ],
-                  // ),
-                  // Divider(height: 8, thickness: 1),
-                  // Wrap(
-                  //   spacing: 5,
-                  //   children: List.generate(_service.categories.length, (index) {
-                  //     return Container(
-                  //       padding: EdgeInsets.symmetric(horizontal: 10, vertical: 4),
-                  //       child: Text(_service.categories.elementAt(index).name, style: Get.textTheme.caption.merge(TextStyle(fontSize: 10))),
-                  //       decoration: BoxDecoration(
-                  //           color: Get.theme.primaryColor,
-                  //           border: Border.all(
-                  //             color: Get.theme.focusColor.withOpacity(0.2),
-                  //           ),
-                  //           borderRadius: BorderRadius.all(Radius.circular(20))),
-                  //     );
-                  //   }),
-                  //   runSpacing: 5,
-                  // ),
-                ],
-              ),
+          ),
+          SizedBox(width: 12),
+          Expanded(
+            child: Wrap(
+              runSpacing: 10,
+              alignment: WrapAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: [
+                    Text(
+                      widget._service.name ?? '',
+                      style: Get.textTheme.bodyText2,
+                      maxLines: 3,
+                      // textAlign: TextAlign.end,
+                    ),
+                  ],
+                ),
+                Divider(height: 8, thickness: 1),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Wrap(
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      spacing: 5,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            if (quantity >= 1) {
+                              setState(() {
+                                quantity = quantity - 1;
+                              });
+                            }
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Get.theme.accentColor,
+                                borderRadius: BorderRadius.circular(4)),
+                            height: 30,
+                            width: 30,
+                            child: Icon(
+                              Icons.remove,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              color: Get.theme.canvasColor,
+                              borderRadius: BorderRadius.circular(4)),
+                          height: 30,
+                          width: 30,
+                          child: Text(quantity.toString()),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            if (quantity >= 0) {
+                              setState(() {
+                                quantity = quantity + 1;
+                              });
+                            }
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Get.theme.accentColor,
+                                borderRadius: BorderRadius.circular(4)),
+                            height: 30,
+                            width: 30,
+                            child: Icon(
+                              Icons.add,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        // SizedBox(
+                        //   height: 32,
+                        //   child: Chip(
+                        //     padding: EdgeInsets.all(0),
+                        //     label: Row(
+                        //       mainAxisAlignment: MainAxisAlignment.center,
+                        //       children: <Widget>[
+                        //         Icon(
+                        //           Icons.star,
+                        //           color: Get.theme.accentColor,
+                        //           size: 18,
+                        //         ),
+                        //         Text(_service.rate.toString(), style: Get.textTheme.bodyText2.merge(TextStyle(color: Get.theme.accentColor, height: 1.4))),
+                        //       ],
+                        //     ),
+                        //     backgroundColor: Get.theme.accentColor.withOpacity(0.15),
+                        //     shape: StadiumBorder(),
+                        //   ),
+                        // ),
+                        // Text(
+                        //   "From (%s)".trArgs([_service.totalReviews.toString()]),
+                        //   style: Get.textTheme.bodyText1,
+                        // ),
+                      ],
+                    ),
+                    Ui.getPrice(widget._service.price,
+                        style: Get.textTheme.headline6),
+                  ],
+                ),
+                // Row(
+                //   children: [
+                //     Icon(
+                //       Icons.build_circle_outlined,
+                //       size: 18,
+                //       color: Get.theme.focusColor,
+                //     ),
+                //     SizedBox(width: 5),
+                //     Flexible(
+                //       child: Text(
+                //         _service.eProvider.name,
+                //         maxLines: 1,
+                //         overflow: TextOverflow.fade,
+                //         softWrap: false,
+                //         style: Get.textTheme.bodyText1,
+                //       ),
+                //     ),
+                //   ],
+                // ),
+                // Row(
+                //   children: [
+                //     Icon(
+                //       Icons.place_outlined,
+                //       size: 18,
+                //       color: Get.theme.focusColor,
+                //     ),
+                //     SizedBox(width: 5),
+                //     Flexible(
+                //       child: Text(
+                //         // TODO eProvider address
+                //         _service.eProvider.firstAddress,
+                //         maxLines: 1,
+                //         overflow: TextOverflow.fade,
+                //         softWrap: false,
+                //         style: Get.textTheme.bodyText1,
+                //       ),
+                //     ),
+                //   ],
+                // ),
+                // Divider(height: 8, thickness: 1),
+                // Wrap(
+                //   spacing: 5,
+                //   children: List.generate(_service.categories.length, (index) {
+                //     return Container(
+                //       padding: EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                //       child: Text(_service.categories.elementAt(index).name, style: Get.textTheme.caption.merge(TextStyle(fontSize: 10))),
+                //       decoration: BoxDecoration(
+                //           color: Get.theme.primaryColor,
+                //           border: Border.all(
+                //             color: Get.theme.focusColor.withOpacity(0.2),
+                //           ),
+                //           borderRadius: BorderRadius.all(Radius.circular(20))),
+                //     );
+                //   }),
+                //   runSpacing: 5,
+                // ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
